b8221f7b8468bb98b6ad6ad0424b5434cb34b500
Jsyuxuan <86676520@qq.com>
2017-11-04 17:27:03 +0800

    gfriend vue

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app" :class="{'nav_open': navOpen}">
+    <G-nav @nav_open="open"></G-nav>
+    <main></main>
+  </div>
+</template>
+
+<script>
+  import GNav from './components/Nav'
+  export default {
+    name: 'app',
+    data () {
+      return {
+        navOpen: false
+      }
+    },
+    components: {
+      GNav
+    },
+    methods: {
+      open () {
+        this.navOpen = true
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  *, *:before, *:after
+    -moz-box-sizing border-box
+    -webkit-box-sizing border-box
+    box-sizing border-box
+  html,body,#app
+    height 100%
+  body
+    margin 0
+    background-color #060606
+  #app
+    transform: translate3d(0, 0, 0);
+    transition: transform 287ms cubic-bezier(0.215, 0.61, 0.355, 1);
+  .nav_open
+    transform translateX(300px)!important
+  main
+    position relative
+    height 100%
+    padding-left 100px
+    overflow hidden
+    z-index 6
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,55 @@
+<template>
+  <nav>
+    <div class="nav_bar" @click="_nav_open"></div>
+    <div class="nav_content">
+      <ul>
+        <li>111111111</li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    _nav_open () {
+      this.$emit('nav_open')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus">
+  nav
+    position fixed
+    left 0
+    top 0
+    width 100px
+    height 100%
+    z-index 800
+    transform translate3d(0, 0, 0)
+    .nav_bar
+      position relative
+      width 100px
+      height 100%
+      padding-top 2.125em
+      background-color #212121
+      z-index 9999
+      cursor pointer
+    .nav_content
+      position absolute
+      width 300px
+      height 100%
+      right 100px
+      top 0
+      background-color #060606
+      transform translate3d(0, 0, 0)
+      transition opacity 487ms ease-out 123ms
+</style>
